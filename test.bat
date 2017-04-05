8e4633a8cd2ce8041b9c30bca7455d176662d820
Mikel Matticoli <mikelmm99@gmail.com>
2017-04-05 15:01:14 -0400

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files (x86)\Google\Chrome\Application\" 
+.\chrome.exe --kiosk --host-rules="MAP * https://docs.google.com/forms/d/e/1FAIpQLSciRyvuvhoaz-iZUJTEMhpS-yP3xuZL2dDiiMb6kPqA0LGQgA/viewform"
